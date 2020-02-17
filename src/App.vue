09d4f2f1de749c3aac62a68d7b5453d16afd6a41
TSmith <tom.smith2711@gmail.com>
2020-02-17 01:02:56 +0000

    Add fake login page

--- src/App.vue
+++ src/App.vue
@@ -15,12 +15,40 @@
           <!--    <b-nav-item href="#">Blogs</b-nav-item>
           <b-nav-item href="#">Bins</b-nav-item>
           <b-nav-item href="#">Composting</b-nav-item>-->
-          <b-nav-item-dropdown text="Admin" right>
+          <b-nav-item-dropdown v-if="isLoggedIn" text="Admin" right>
             <b-dropdown-item :to="{name : 'product-admin'}">Product Admin</b-dropdown-item>
             <b-dropdown-item :to="{name : 'shop-admin'}">Shop Admin</b-dropdown-item>
           </b-nav-item-dropdown>
         </b-navbar-nav>
       </b-collapse>
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item v-if="!isLoggedIn">
+          <b-button v-b-modal.login-modal variant="primary">Login</b-button>
+          <b-modal hide-footer title="Login" centered id="login-modal">
+            <b-form @submit.prevent="onSubmit">
+              <b-form-group id="input-group-1" label="Email address:" label-for="input-1">
+                <b-form-input id="input-1" v-model="form.email" type="email" required></b-form-input>
+              </b-form-group>
+
+              <b-form-group id="input-group-2" label="Password" label-for="input-2">
+                <b-form-input id="input-2" v-model="form.password" type="password" required></b-form-input>
+              </b-form-group>
+
+              <div class="d-flex justify-content-between w-100">
+                <button class="btn btn-secondary" type="button" @click="$bvModal.hide('login-modal')">Cancel</button>
+                <button class="btn btn-primary" type="submit">Login</button>
+              </div>
+            </b-form>
+          </b-modal>
+        </b-nav-item>
+        <b-nav-item-dropdown v-else right>
+          <!-- Using 'button-content' slot -->
+          <template v-slot:button-content>
+            <em>{{user.email}}</em>
+          </template>
+          <b-dropdown-item @click="logout">Log out</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
     </b-navbar>
     <router-view></router-view>
   </div>
@@ -29,11 +57,41 @@
 <script lang="ts">
 import Vue from 'vue';
 import HelloWorld from './components/HelloWorld.vue';
+import { mapGetters } from 'vuex';
 export default Vue.extend({
   name: 'app',
   components: {
     HelloWorld,
   },
+  computed: {
+    ...mapGetters(['isLoggedIn', 'user'])
+  },
+  data() {
+    return {
+      form: {
+        email: '',
+        password: '',
+      },
+    }
+  },
+  created() {
+    var user = window.localStorage.getItem('recycle-user');
+    if (user != undefined) {
+      this.$store.commit('setUser', {
+        email: user
+      })
+    }
+  },
+  methods: {
+    logout() {
+      this.$store.dispatch('LOGOUT')
+    },
+    async onSubmit() {
+      await this.$store.dispatch('LOGIN')
+      this.$bvModal.hide('login-modal')
+    },
+
+  }
 });
 </script>
 
--- src/store/index.ts
+++ src/store/index.ts
@@ -4,8 +4,37 @@ import Vuex from 'vuex';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-	state: {},
-	mutations: {},
-	actions: {},
-	modules: {}
+	state: {
+		user: {
+			email: ''
+		}
+	},
+	mutations: {
+		setUser(state, user) {
+			state.user = user;
+		}
+	},
+	actions: {
+		LOGIN({ commit }) {
+			window.localStorage.setItem('recycle-user', 'poppy');
+			commit('setUser', {
+				email: 'poppy'
+			});
+		},
+		LOGOUT({ commit }) {
+			window.localStorage.removeItem('recycle-user');
+			commit('setUser', {
+				email: ''
+			});
+		}
+	},
+	modules: {},
+	getters: {
+		isLoggedIn(state) {
+			return state.user.email.length > 0;
+		},
+		user(state) {
+			return state.user;
+		}
+	}
 });
